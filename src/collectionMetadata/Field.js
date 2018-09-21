1e77426d07c0083ca1df1945947769f27b4ae379
dkirchhof <d.kirchhof@learning-by.de>
2018-09-21 09:45:55 +0200

    separate src and dist folder; create declaration files

--- src/collectionMetadata/Field.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Field {
-    constructor() {
-        this.__type = "field";
-    }
-}
-exports.Field = Field;
--- src/decorators/collectionDecorators.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Store_1 = require("../Store");
-function CollectionDescriptor(collectionName) {
-    return function (target) {
-        Store_1.arangoStore.registerDocumentCollection(target, collectionName);
-    };
-}
-exports.CollectionDescriptor = CollectionDescriptor;
